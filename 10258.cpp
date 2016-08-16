6626aae473cce5120934ae5d600396077ee6aecd
GustavoAC <guga.a.carvalho@gmail.com>
2016-08-16 11:20:21 -0300

    Update 10258.cpp

--- 10258.cpp
+++ 10258.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <algorithm>
+#include <cstring>
+
+// Penalty is only added if the question is solved later
 
 struct Team {
+	int id;
 	int accepted = 0;
 	int penalty = 0;
 	bool participating = false;
@@ -10,7 +15,8 @@ struct Team {
 };
 
 bool compare(const Team &a, const Team &b) {
-	return a.participating and (a.accepted > b.accepted or (a.accepted == b.accepted and a.penalty < b.penalty));
+	return a.participating and (a.accepted > b.accepted or (a.accepted == b.accepted and a.penalty < b.penalty) or
+		   a.accepted == b.accepted and a.penalty == b.penalty and a.id < b.id);
 }
 
 int main(int argc, char const *argv[]) {
@@ -22,26 +28,41 @@ int main(int argc, char const *argv[]) {
 
 	while (std::cin >> numCompetitions and numCompetitions) {
 		std::cin.ignore();
-		for (int i = 0; i < numCompetitions; ++i) {
-			while (std::getline(std::cin, textInfo) and textInfo != "\n") {
+		std::getline(std::cin, textInfo);	//Gets rid of the first newline
+		while (numCompetitions--) {
+			while (std::getline(std::cin, textInfo) and textInfo != "") {
 				std::stringstream ss(textInfo);
 				ss >> teamId >> questId >> timeSpent >> statusSub;
+				teams[teamId].id = teamId;
 				teams[teamId].participating = true;
 
 				if (statusSub == 'C' and !teams[teamId].solvedQuestions[questId]) {
 					teams[teamId].accepted++;
 					teams[teamId].solvedQuestions[questId] = true;
 					teams[teamId].penalty += timeSpent;
-				} else if (statusSub == 'I' and !teams[teamId].solvedQuestions[questId])
+				} else if (statusSub == 'I' and !teams[teamId].solvedQuestions[questId]){
 					teams[teamId].penalty += 20;
+				}
+				
 			}
+
 
 			std::sort(teams.begin() + 1, teams.end(), compare);
 
-			for (int i = 1; i <= 100; ++i) {
-				// imprime só os corretos e zera os não zerados
-			}
+			for (int i = 1; i <= 100; ++i)
+				if (teams[i].participating){
+					std::cout << teams[i].id << " " << teams[i].accepted << " ";
+					if(teams[i].accepted) std::cout << teams[i].penalty << "\n";
+					else std::cout << "0\n";
 
+					teams[i].participating = false;
+					teams[i].accepted = 0;
+					teams[i].penalty = 0;
+					for (int j = 0; j < 10; j++)
+						teams[i].solvedQuestions[j] = false;
+				}
+
+			if (numCompetitions) std::cout << "\n";
 		}
 	}
 
